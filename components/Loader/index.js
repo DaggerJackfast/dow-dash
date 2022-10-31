9c91872c7911dfc4739338312a70d95a6141631d
Aibek <aibek.abdykasymov@gmail.com>
2022-11-01 00:08:22 +0600

    added loader component

---
+++ components/Loader/index.js
@@ -0,0 +1,10 @@
+const Loader = ({text}) => (
+    <div className="relative h-64 w-64">
+      <div className="border-t-slate-700 border-solid animate-spin rounded-full border-slate-200 border-8 h-64 w-64" />
+      <div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 text-slate-700 animate-pulse">{text}</div>
+    </div>
+)
+Loader.defaultProps = {
+  text: 'Loading'
+}
+export default Loader;
