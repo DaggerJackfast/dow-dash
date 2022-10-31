4a4bdbad59f15ffda825c485afc2df89dbe022ce
Aibek <aibek.abdykasymov@gmail.com>
2022-11-01 00:06:04 +0600

    added rewrites to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const rewrites = async () => {
+  const apiPath = process.env.NEXT_PUBLIC_API_PATH;
+  const publicApiPath = process.env.API_URL;
+  return [
+    {
+      source: `${apiPath}/:path*`,
+      destination: `${publicApiPath}/:path*`
+    }
+  ]
+}
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites
 }
 
 module.exports = nextConfig
