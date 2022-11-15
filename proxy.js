4ab37b020f9a90ba285327a08babdbe854c89b1e
Aibek <aibek.abdykasymov@gmail.com>
2022-11-15 19:54:23 +0600

    fixed using proxy

--- proxy.js
+++ proxy.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const next = require('next');
-const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
+const { createProxyMiddleware} = require('http-proxy-middleware');
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const apiPath = process.env.NEXT_PUBLIC_API_PATH || '/api'
 const app = next({dev})
 const handle = app.getRequestHandler();
 
 
 app.prepare().then(() => {
   const server = express();
-  const destinationApiUrl = process.env.API_URL;
+  const apiPath = process.env.API_URL || '/api';
+  const proxyServer = process.env.PROXY_SERVER;
+  const proxyPath = process.env.PROXY_PATH;
+
+  const options = {
+    target: proxyServer,
+    changeOrigin: true,
+    logLevel: 'debug',
+    pathRewrite: {
+      [`^${apiPath}`]: proxyPath,
+    }
+  }
   if (dev) {
-    server.use(apiPath, proxy({
-        target: destinationApiUrl,
-        changeOrigin: true,
-      })
-    );
+    const proxyMiddleware = createProxyMiddleware(options);
+    server.use(apiPath, proxyMiddleware);
   }
   server.all('*', (req, res) => handle(req, res))
   server.listen(port, (err) => {
