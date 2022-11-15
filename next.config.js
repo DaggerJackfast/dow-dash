fc586009530d6c5443a42323290376da6a35e3bb
Aibek <aibek.abdykasymov@gmail.com>
2022-11-15 19:53:47 +0600

    deleted rewrites from next config, added runtime env config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 /** @type {import('next').NextConfig} */
-const rewrites = async () => {
-  const apiPath = process.env.NEXT_PUBLIC_API_PATH;
-  const publicApiPath = process.env.API_URL;
-  return [
-    {
-      source: `${apiPath}/:path*`,
-      destination: `${publicApiPath}/:path*`
-    }
-  ]
-}
 const nextConfig = {
+  publicRuntimeConfig: {
+    apiPath: process.env.API_URL,
+    websocketHost: process.env.WEBSOCKET_HOST,
+    mode: process.env.NODE_ENV
+  },
   reactStrictMode: true,
   swcMinify: true,
-  rewrites
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
