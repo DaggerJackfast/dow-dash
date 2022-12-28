c992fc97ec928faef240328a33742bdcefe33de2
Aibek <aibek.abdykasymov@gmail.com>
2022-12-28 15:25:37 +0600

    added padding to tab body

--- components/Tab/TabBody/index.js
+++ components/Tab/TabBody/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import cx from "classnames";
 import PropTypes from "prop-types";
 import { childrenProps } from "../../../lib/prop-types";
 
 export const TabBody = ({ children, className }) => {
-  return <div className={className}>{children}</div>;
+  return (
+    <div className={cx("pt-20 pb-20", { [className]: !!className })}>
+      {children}
+    </div>
+  );
 };
 TabBody.propTypes = {
   children: childrenProps.isRequired,
