e1792966eedc986fadb384581ecc84d7b6b9b178
Aibek <aibek.abdykasymov@gmail.com>
2023-01-04 15:05:03 +0600

    added document page file

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => (
+  <Html>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+      <div id="modal-root" />
+    </body>
+  </Html>
+);
+export default Document;
